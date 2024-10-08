fa4bd7108d03e6ca06780d698546a9e6fe812b6e
Samyak <samyak15.sj@gmail.com>
2024-10-08 23:14:53 +0530

    added mobile responsiveness for landing page

--- script.js
+++ script.js
@@ -174,4 +174,9 @@ if (marquee) {
 
   
 
-
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('nav ul');
+  
+  navToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+  });
