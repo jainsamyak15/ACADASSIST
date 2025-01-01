e653a31b258e1eed74fc2f71fbdb488941b0cf3e
Samyak <samyak15.sj@gmail.com>
2025-01-01 23:56:27 +0530

    ...

--- js/supabase.js
+++ js/supabase.js
@@ -1,13 +1,7 @@
-
-
-
 // js/supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-if (!supabaseUrl || !supabaseAnonKey) {
-    console.error('Missing Supabase credentials')
-}
+const supabaseUrl = 'https://zvapwryhkczxlfwfdjrd.supabase.co'  // Replace with your actual URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2YXB3cnloa2N6eGxmd2ZkanJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MTQ4ODIsImV4cCI6MjA1MTI5MDg4Mn0.8CFbvGPOzz3A9UdKseEXyun_j7_lfVMZI1aYjbLp4n0'  // Replace with your actual key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
